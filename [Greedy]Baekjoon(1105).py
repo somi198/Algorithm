df3953863f1a12df8d7c58ae9da3de50cb55ffda
김소미 <somi198@naver.com>
2022-07-19 17:17:58 +0900

    Update [Greedy]Baekjoon(1105).py

--- [Greedy]Baekjoon(1105).py
+++ [Greedy]Baekjoon(1105).py
@@ -1,11 +1,13 @@
-L, R = input().split()
-L_len, R_len = len(L), len(R)
-answer = 0
-
-for i in range(R_len):
-    if L_len == R_len:
+def solution(L, R):
+    if len(L) != len(R):
+       return 0
+		
+    answer = 0
+    for i in range(len(R)):
         if L[i] == R[i]:
             if L[i] == '8': answer += 1
         else: break
+    return answer
 
-print(answer)
+L, R = input().split()
+print(solution(L, R))
