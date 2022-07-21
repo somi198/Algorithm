05be98bd25eb125df95e716a481a32f176e59b5b
김소미 <somi198@naver.com>
2022-07-21 21:46:21 +0900

    Create [Greedy]Baekjoon(9440).py

---
+++ [Greedy]Baekjoon(9440).py
@@ -0,0 +1,24 @@
+from collections import deque
+
+cmd = input().split()
+
+while cmd[0] != '0':
+    N = int(cmd[0])
+    number = deque(sorted(cmd[1:]))
+    n1, n2 = '', ''
+    
+    while number:    
+        if not n1 and not n2:
+            zero = 0
+            while not len(n1)*len(n2) and number:
+                n = number.popleft()
+                if n == '0': zero += 1
+                elif not n1: n1 += n
+                else: n2 += n
+            number = deque(['0']*zero) + number
+        else:
+            if number: n1 += number.popleft()
+            if number: n2 += number.popleft()
+    
+    print(int(n1)+int(n2))
+    cmd = input().split()
