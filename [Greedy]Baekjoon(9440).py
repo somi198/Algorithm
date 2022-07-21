e739e816094cb03a2f1b77ef6db234f077cd9255
김소미 <somi198@naver.com>
2022-07-21 21:54:40 +0900

    Update [Greedy]Baekjoon(9440).py

--- [Greedy]Baekjoon(9440).py
+++ [Greedy]Baekjoon(9440).py
@@ -8,17 +8,16 @@ while cmd[0] != '0':
     n1, n2 = '', ''
     
     while number:    
-        if not n1 and not n2:
-            zero = 0
-            while not len(n1)*len(n2) and number:
-                n = number.popleft()
-                if n == '0': zero += 1
-                elif not n1: n1 += n
-                else: n2 += n
-            number = deque(['0']*zero) + number
-        else:
-            if number: n1 += number.popleft()
-            if number: n2 += number.popleft()
+        zero = 0
+        while not len(n1)*len(n2) and number:
+            n = number.popleft()
+            if n == '0': zero += 1
+            elif not n1: n1 += n
+            else: n2 += n
+            
+        number = deque(['0']*zero) + number
+        if number: n1 += number.popleft()
+        if number: n2 += number.popleft()
     
     print(int(n1)+int(n2))
     cmd = input().split()
