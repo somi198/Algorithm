ebff0d2e10d5187e943c62379759baced4707b11
김소미 <somi198@naver.com>
2022-07-29 14:10:36 +0900

    Update [DP]Baekjoon(12026).py

--- [DP]Baekjoon(12026).py
+++ [DP]Baekjoon(12026).py
@@ -1,13 +1,13 @@
 N = int(input())
 block = input()
 dp = [0]*N
+dp[0] = 1
 
 for i in range(N):
     for j in range(i):
-        if block[i]+block[j] in ('BJ', 'OB', 'JO'):
-            if j==0 or dp[j]:
-                if dp[i]: dp[i] = min(dp[i], dp[j]+(i-j)**2)
-                else: dp[i] = dp[j]+(i-j)**2
+        combi = block[i]+block[j]
+        if combi in ('BJ', 'OB', 'JO') and dp[j]:
+            if dp[i]: dp[i] = min(dp[i], dp[j]+(i-j)**2)
+            else: dp[i] = dp[j]+(i-j)**2
 
-if dp[-1]: print(dp[-1])
-else: print(-1) 
+print(dp[-1]-1)
